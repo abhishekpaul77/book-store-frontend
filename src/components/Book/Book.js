7704a62a81ed219bca340b606a03b65b76a4e9a6
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2023-08-23 14:35:59 +0530

    Update Book.js

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -7,7 +7,7 @@ const Book = (props) => {
   const { _id, name, author, description, price, image } = props.book;
   const deleteHandler = async () => {
     await axios
-      .delete(`http://localhost:5000/books/${_id}`)
+      .delete(`https://book-store-b7fq.onrender.com/books/${_id}`)
       .then((res) => res.data)
       .then(() => history("/"))
       .then(() => history("/books"));
