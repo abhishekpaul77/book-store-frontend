05fcff9726c7a1d6f199b35ba7444c0b40df29d2
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2023-08-23 14:48:57 +0530

    Update BookDetail.js

--- src/components/Book/BookDetail.js
+++ src/components/Book/BookDetail.js
@@ -18,7 +18,7 @@ const BookDetail = () => {
   useEffect(() => {
     const fetchHandler = async () => {
       await axios
-        .get(`https://book-store-b7fq.onrender.com/books/${_id}`)
+        .get(`https://book-store-b7fq.onrender.com/books/${id}`)
         .then((res) => res.data)
         .then((data) => setInputs(data.book));
     };
@@ -27,7 +27,7 @@ const BookDetail = () => {
 
   const sendRequest = async () => {
     await axios
-      .put(`https://book-store-b7fq.onrender.com/books/${_id}`, {
+      .put(`https://book-store-b7fq.onrender.com/books/${id}`, {
         name: String(inputs.name),
         author: String(inputs.author),
         description: String(inputs.description),
