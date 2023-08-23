3608fb9b2aebce5341ebbe31af5a2b7e9a471101
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2023-08-23 13:46:29 +0530

    Update AddBook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -32,7 +32,7 @@ const AddBook = () => {
 
   const sendRequest = async () => {
     await axios
-      .post("https://book-store-9n3f.onrender.com/books", {
+      .post("https://book-store-z7xt.onrender.com", {
         name: String(inputs.name),
         author: String(inputs.author),
         description: String(inputs.description),
